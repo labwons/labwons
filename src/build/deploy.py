265c89cb93a459b681061245a03f88acfb5cb26d
JeHyeuk <JeHyeuk.Lee@hyundai-kefico.com>
2026-01-26 09:24:45 +0900

    BUG FIX

--- src/build/deploy.py
+++ src/build/deploy.py
@@ -158,12 +158,12 @@ if __name__ == "__main__":
     # UPDATE AFTER MARKET DATA
     # ---------------------------------------------------------------------------------------
     if GITHUB.CONFIG.AFTERMARKET:
-        try:
-            afterMarket = AfterMarket()
-            afterMarket.data.to_parquet(FILE.AFTER_MARKET, engine='pyarrow')
-            context += [f"- [{afterMarket.state}] UPDATE AFTER MARKET: ", afterMarket.log, ""]
-        except Exception as report:
-            context += [f"- [FAILED] UPDATE AFTER MARKET: ", f'{report}', ""]
+        # try:
+        afterMarket = AfterMarket()
+        afterMarket.data.to_parquet(FILE.AFTER_MARKET, engine='pyarrow')
+        context += [f"- [{afterMarket.state}] UPDATE AFTER MARKET: ", afterMarket.log, ""]
+        # except Exception as report:
+        #     context += [f"- [FAILED] UPDATE AFTER MARKET: ", f'{report}', ""]
     else:
         context += [f"- [PASSED] UPDATE AFTER MARKET: ", ""]
 
